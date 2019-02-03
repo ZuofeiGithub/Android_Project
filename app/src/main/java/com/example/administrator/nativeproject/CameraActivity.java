435d422dab35c170709868a7184d41bc3c8d72df
ZuofeiGithub <348068347@qq.com>
2019-02-03 10:35:41 +0800

    打开系统相机

---
+++ app/src/main/java/com/example/administrator/nativeproject/CameraActivity.java
@@ -0,0 +1,76 @@
+package com.example.administrator.nativeproject;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CameraActivity extends AppCompatActivity {
+    private static int REQ_1 = 1;
+    private static int REQ_2 = 2;
+    private ImageView iv;
+    private String mFilePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        iv = findViewById(R.id.iv);
+        mFilePath = Environment.getExternalStorageDirectory().getPath();//获取SDK路径
+        mFilePath = mFilePath + "/"+"temp.png";
+    }
+
+       public void startCamera1(View view){
+        Toast.makeText(this,"打开相机",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivity(intent); //启动无返回数据
+        startActivityForResult(intent,REQ_1);
+    }
+    public void startCamera2(View view){
+        Toast.makeText(this,"打开相机",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoUri = Uri.fromFile(new File(mFilePath));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);//系统拍照之后存储的数据路径进行更改成自己的路径
+        //startActivity(intent); //启动无返回数据
+        startActivityForResult(intent,REQ_2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQ_1){
+                Bundle bundle = data.getExtras();//封装的所有返回的数据
+                Bitmap bitmap = (Bitmap) bundle.get("data");
+                iv.setImageBitmap(bitmap);//缩略图
+            }else if(requestCode == REQ_2){
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(mFilePath);
+                    Bitmap bitmap = BitmapFactory.decodeStream(fis);
+                    iv.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/administrator/nativeproject/fragment/ContentFragment.java
+++ app/src/main/java/com/example/administrator/nativeproject/fragment/ContentFragment.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.administrator.nativeproject.CameraActivity;
 import com.example.administrator.nativeproject.MainActivity;
 import com.example.administrator.nativeproject.R;
 
@@ -16,10 +18,20 @@ public class ContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_content,null);
         Button addfragBtn = view.findViewById(R.id.add_fragment);
+        Button startCamera = view.findViewById(R.id.start_camera);
+        startCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"打开摄像机",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setClass(getActivity(),CameraActivity.class);
+                startActivity(intent);
+            }
+        });
         addfragBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(getActivity(),"动态添加fragment",Toast.LENGTH_SHORT).show();
             }
         });
 
